e015c55519fac7ed0a8b8a52f52ee3d41c8509f0
LizzieJ <magpie.kreuz@gmail.com>
2025-02-06 00:01:26 -0600

    agregarnombre

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Creación de array para almacenar nombres
+
+let listaAmigos = [];
+
+
+// Función para agregar nombres de amigos
+ function agregarAmigo(){
+    let nombre = document.getElementById('amigo').value;
+    console.log(nombre);
+
+    // Validar que hay algo escrito
+    if (nombre !== '') {
+        listaAmigos.push(nombre)
+        actualizarLista ();
+        limpiarCaja ();
+    } else {
+        alert ('Por favor, inserte un nombre');
+        return;
+    }
+ }
+
+ // función para limpiar caja
+ function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
+
+//función para agregar nombres a la lista visible
+/*function actualizarLista () {
+    let lista = document.querySelector('#listaAmigos');
+    for (let i = 0; i <listaAmigos.lenght; i++) {
+        let li = document.createElement('li');
+        li.textContent = listaAmigos[i];
+        lista.appendChild(li);
+    }
+}
